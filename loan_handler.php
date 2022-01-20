98f3555f8f02d6e9ece219d7877a5403d57b81e2
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-19 19:17:18 -0600

    2nd commit

--- loan_handler.php
+++ loan_handler.php
@@ -1,4 +1,4 @@
-
+<!doctype html="en">
 
 <title>Loan Enquiry</title>
 
@@ -12,56 +12,75 @@
  <body>
      <main>
         <?php
-            $name = isset($_POST['fullname']);
-             $email = isset($_POST['emailaddress']);
-            $employment = isset($_POST['employment']);
-            $age =isset($_POST['age']);
+            $name = $_POST['fullname'];
+            $email = $_POST['emailaddress'];
+            $employment = $_POST['employment'];
+            $age = $_POST['age'];
           
-             if(isset($_POST['fullname'])) {
-                 if(!empty($_POST['fullname'])){
-                     $name = stripslashes($_POST['fullname']);
-                 }else {
-                     $name = NULL;
-                     echo'<p><font color="red">Please enter your name</font></p>';
-                 }
-                $name = $_POST['fullname'];
+             if ($_POST['fullname']) 
+             {
+                        if  (!empty($_POST['fullname'])){
+                            $name = stripslashes($_POST['fullname']);
+                        }
+                        else
+                         {
+                            $name = NULL;
+                            echo '<p><font color="red">Please enter your name</font></p>';
+                        }
+                            $name = $_POST['fullname'];
             }else {
-                echo("<p>You forgot to select your name</p>");
-                $name = NULL;
+                    echo("<p>You forgot to select your name</p>");
+                    $name = NULL;
             }
-            if(isset($_POST['emailaddress'])){
-                  if(!empty($_POST['emailaddress'])){
-                     $email = stripslashes($_POST['emailaddress']);
-                 }else {
-                     $email = NULL;
-                     echo'<p><font color="red">Please enter your email</font></p>';
-                 }
-                $email = $_POST['emailaddress'];
-            } else {
-                $email = NULL;
+            if  ($_POST['emailaddress'])
+            {
+                         if (!empty($_POST['emailaddress'])){
+                            $email = stripslashes($_POST['emailaddress']);
+                        }
+                        else
+                         {
+                            $email = NULL;
+                            echo '<p id="email">Please enter your email</p>';
+                        }
+                        
+                        $email = $_POST['emailaddress'];
+            } 
+            else 
+            {
+                $email = null;
             }
-            if(isset($_POST['employment'])) {
-                  if(!empty($_POST['employment'])){
-                     $employment = stripslashes($_POST['employment']);
-                 }else {
-                     $employment = NULL;
-                     echo'<p><font color="red">Please enter your Employer</font></p>';
-                 }
-                $employment = $_POST['employment'];
-            }else {
-                echo("<p>You forgot to select your employment state</p>");
-                $employment = NULL;
+            if ($_POST['employment']) 
+            {
+                            if (!empty($_POST['employment'])){
+                                $employment = stripslashes($_POST['employment']);
+                            }
+                            else
+                             {
+                                $employment = null;
+                                echo'<p id="employed">Please enter your Employer</p>';
+                            }
+                                $employment = $_POST['employment'];
             }
-            if(isset($_POST['age'])){
-                  if(!empty($_POST['age'])){
-                     $age = stripslashes($_POST['age']);
-                 }else {
-                     $age = NULL;
-                     echo'<p><font color="red">Please enter your age</font></p>';
-                 }
-                $age =$_POST['age'];
-            }else {
-                $age = NULL;
+            else 
+            {
+                                echo("<p>You forgot to select your employment state</p>");
+                                $employment = null;
+            }
+            if ($_POST['age'])
+            {
+                                if(!empty($_POST['age'])){
+                                    $age = stripslashes($_POST['age']);
+                                }
+                                else 
+                                {
+                                    $age = null;
+                                    echo'<p>Please enter your age</p>';
+                                }
+                                    $age =$_POST['age'];
+            }
+            else
+            {
+                                    $age = NULL;
             }
             echo "<p>Thank you, <b>$name</b>, for tour enquery</p>
             <p>We will reply to you at your email address: <strong>$email</strong>.</p>";
@@ -70,4 +89,4 @@
         
        </main>
     </body>
-
+</html>
