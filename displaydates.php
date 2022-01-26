b3bb95ce6b343ee56350899b33bd54b43f3c8cf7
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-26 16:46:14 -0600

    update flex

--- displaydates.php
+++ displaydates.php
@@ -23,31 +23,24 @@ $day = $currentdate["mday"];
 $year = $currentdate["year"];
 ?>
 
-            
 <!DOCTYPE html>
-
 <html>
     <head>
-      <!DOCTYPE html>
-         <script>
+      
+ <script>
 let myWindow;
 function closeWin() {
   myWindow.close();
  }
 </script>
-<html>
+
 <body>
- <button class="btn btn-primary" type="button" onclick="closeWin()">Close</button>
-
-    
  
-
 </body>
 </html>
 
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Calculations</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -93,8 +86,11 @@ function closeWin() {
                     <?php
                      }
             ?>
+           
             </select>
+            
         </div>
+         <button class="btn btn-primary button1" type="button" onclick="open(location, '_self').close();">Close</button>
 </div>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>   
        
